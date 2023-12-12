389a6d6185dad72a2e79df2803bcd9328a221bbb
Kristian75 <152974653+Kristian75@users.noreply.github.com>
2023-12-12 12:31:03 +0100

    Add files via upload

---
+++ vjezba02.cs
@@ -0,0 +1,19 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int a=10;
+		int b=7;
+		double c=12.5;
+		double d=16.7;
+		
+		double prosjek;
+		
+		prosjek=(a+b+c+d)/4;
+		
+		Console.WriteLine("Rezultat je: {0}", +prosjek);
+		
+	}
+}
